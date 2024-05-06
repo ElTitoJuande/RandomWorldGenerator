82eaa63cdba61b43e984d3b01207a068481d0446
ElTitoJuande <juandepm00@gmail.com>
2024-05-06 14:08:36 +0200

    Primer Commit de mi vida

---
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -0,0 +1,9 @@
+package randomworldgenerator;
+
+public class RandomWorldGenerator {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Jose 'El Guapetón'");
+    }
+    
+}
