45080ab159063f146641cac86a7266482a9581d4
ElTitoJuande <168193796+ElTitoJuande@users.noreply.github.com>
2024-05-13 14:10:50 +0200

    Update RandomWorldGenerator.java

--- RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
+++ RandomWorldGenerator/src/randomworldgenerator/RandomWorldGenerator.java
@@ -5,6 +5,7 @@ public class RandomWorldGenerator {
     public static void main(String[] args) {
         System.out.println("Hola Jose 'El Guapetón'");
         System.out.println("Adios");
+        System.out.println("Hola2");
     }
     
 }
